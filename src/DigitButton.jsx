26785a0bba32eafe2d18500a64516d8b55b1a415
Deepali kumari <deepalikumari1234@gmail.com>
2024-05-09 16:24:37 +0530

    task

---
+++ src/DigitButton.jsx
@@ -0,0 +1,12 @@
+
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
